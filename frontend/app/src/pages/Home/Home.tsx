e3f2d5547ab003fa2a0185c7924899ed5a1130b4
axelf <axelf@theodo.fr>
2023-10-13 16:00:41 +0200

    penalty svg

--- frontend/app/src/pages/Home/Home.tsx
+++ frontend/app/src/pages/Home/Home.tsx
@@ -53,6 +53,78 @@ const Home = (): JSX.Element => {
       height="100vh"
       maxWidth="100%"
     >
+      <div
+        style={{
+          position: 'absolute',
+          right: '10px',
+        }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="800" height="600">
+          <g>
+            <title>Layer 1</title>
+            <line
+              strokeWidth="17"
+              id="svg_2"
+              y2="310"
+              x2="527.03941"
+              y1="310"
+              x1="324"
+              stroke="#c0cde0"
+              fill="none"
+            />
+            <line
+              transform="rotate(90 315.52 254)"
+              stroke="#c0cde0"
+              strokeWidth="17"
+              id="svg_3"
+              y2="253.99997"
+              x2="514.03944"
+              y1="253.99997"
+              x1="117.00002"
+              fill="none"
+            />
+            <line
+              transform="rotate(90 535.52 254)"
+              stroke="#c0cde0"
+              strokeWidth="17"
+              id="svg_4"
+              y2="253.99997"
+              x2="734.03944"
+              y1="253.99997"
+              x1="337.00002"
+              fill="none"
+            />
+            <line
+              strokeWidth="18"
+              strokeDasharray="5,5"
+              stroke="#bc0101"
+              id="svg_5"
+              y2="180"
+              x2="420"
+              y1="500"
+              x1="420"
+              fill="none"
+            >
+              <animate
+                attributeName="x2"
+                values="200;640;200"
+                dur="10s"
+                repeatCount="indefinite"
+              />
+            </line>
+            <ellipse
+              transform="rotate(-12.1963 427.517 522.577)"
+              stroke="#ffffff"
+              ry="51.5"
+              rx="30.59841"
+              id="svg_1"
+              cy="522.5771"
+              cx="427.51666"
+              fill="#ffffff"
+            />
+          </g>
+        </svg>
+      </div>
       {bunkerPollUpdated?.onBunkerPollUpdated.isActive === true && (
         <>
           <div
