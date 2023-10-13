7b173fc321cc370bc3b8fa04b5e490b43bba998b
Guillaume Duboc <guillaumed@theodo.fr>
2023-10-13 14:03:02 +0200

    feat: fix frontend build

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from '@apollo/client';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/system';
+import { useEffect, useState } from 'react';
 import { IntlProvider } from 'react-intl';
 
 import { startChatBot } from 'chatbot/chatbot';
@@ -38,26 +39,44 @@ export const createAppSyncApolloClient = async ({
     connectToDevTools,
   });
 
-const client = await createAppSyncApolloClient({
-  appSyncApiUrl:
-    'https://q42jhugczngnlafxcv27nc676e.appsync-api.eu-west-1.amazonaws.com/graphql',
-  getJwtToken: () => Promise.resolve('da2-zrz3ahlckzcefb7uxkc73nrhja'),
-  cacheConfig: {},
-  connectToDevTools: true,
-});
-
 const App = (): JSX.Element => {
   startChatBot();
 
+  const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
+
+  useEffect(() => {
+    const instanciateClient = async () => {
+      setClient(
+        await createAppSyncApolloClient({
+          appSyncApiUrl:
+            'https://q42jhugczngnlafxcv27nc676e.appsync-api.eu-west-1.amazonaws.com/graphql',
+          getJwtToken: () => Promise.resolve('da2-zrz3ahlckzcefb7uxkc73nrhja'),
+          cacheConfig: {},
+          connectToDevTools: true,
+        }),
+      );
+    };
+    void instanciateClient();
+
+    return () => {
+      client?.stop();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={muiTheme}>
-        <IntlProvider locale="fr-FR" messages={intlMessages}>
-          <CssBaseline />
-          <AppRoutes />
-        </IntlProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+    <>
+      {client !== undefined && (
+        <ApolloProvider client={client}>
+          <ThemeProvider theme={muiTheme}>
+            <IntlProvider locale="fr-FR" messages={intlMessages}>
+              <CssBaseline />
+              <AppRoutes />
+            </IntlProvider>
+          </ThemeProvider>
+        </ApolloProvider>
+      )}
+    </>
   );
 };
 
